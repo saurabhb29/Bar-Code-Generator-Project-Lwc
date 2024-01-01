c02e98487aff8de0f3514880027fbef196db37d5
Saurabh Bisht <1999saurabhbisht@gmail.com>
2024-01-01 17:16:44 +0530

    newbies

--- force-app/main/default/lwc/barCodeGeneratorComponent/barCodeGeneratorComponent.js
+++ force-app/main/default/lwc/barCodeGeneratorComponent/barCodeGeneratorComponent.js
@@ -10,26 +10,26 @@ export default class BarCodeGeneratorComponent extends LightningElement {
     // Expose the recordId as an API property to make it accessible in the Lightning App Builder
     @api recordId;
    // Declare a variable to store the product name.
-    productName;
+productName;
 
 // Wire the getRecordName function to retrieve data based on the provided recordId.
-    @wire(getRecordName, { recordId: '$recordId' })
-    wiredRecordName({ data, error }) {
+@wire(getRecordName, { recordId: '$recordId' })
+wiredRecordName({ data, error }) {
     // Check if data is available (successful response).
-        if (data) {
+    if (data) {
         // Assign the retrieved data to the productName variable.
-            this.productName = data;
+        this.productName = data;
         // Reset the error variable to undefined in case it was previously set.
-            this.error = undefined;
+        this.error = undefined;
     } 
     // Check if there is an error in the response.
     else if (error) {
         // Set productName to undefined in case of an error.
-            this.productName = undefined;
+        this.productName = undefined;
         // Extract and store the error message in the error variable.
-            this.error = error.body.message;
-        }
+        this.error = error.body.message;
     }
+}
     // Flag to track whether the script has been loaded or not
     isScriptLoaded = false;
 
